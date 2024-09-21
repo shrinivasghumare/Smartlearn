be7cc50c44d745c81c71703dfb4bfc7afac4b0b8
Prashant Singh <113935621+Prash-is-alive@users.noreply.github.com>
2024-09-21 20:22:30 +0530

    Improved UI

--- src/app/(components)/_navbar/page.jsx
+++ src/app/(components)/_navbar/page.jsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useContext } from "react";
 import LayoutContext from "../../context/LayoutContext";
+import Image from "next/image";
+import logo from "../../assets/vidyagram_logo.png";
 import "./styles.css";
 const NavBar = () => {
-  const { router, user, setUser } = useContext(LayoutContext);
+  const { router, user } = useContext(LayoutContext);
   const pathName = usePathname();
-
   const navLinks = [
     { href: "/", text: "Home" },
     { href: "/pages/videos", text: "Videos" },
@@ -15,7 +16,6 @@ const NavBar = () => {
     { href: "/pages/notes", text: "Notes" },
     { href: "/pages/assignments", text: "Assignments" },
   ];
-
 
   const navbarStyle = {
     top: 0,
@@ -35,53 +35,55 @@ const NavBar = () => {
         style={navbarStyle}
       >
         <div className="container-fluid">
-          <Link className="navbar-brand" href="/">
-            {user?.username}
+          <Link className="navbar-brand d-flex align-items-center" href="/">
+          <Image
+           src={logo}
+           width={50}
+           alt="Picture of the author"
+          ></Image>
+            {"VidyaGram-"+  user?.username}
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {navLinks.map((link) => {
-                const isActive = pathName.startsWith(link.href);
-                return (
-                  <li className="nav-item" key={link.href}>
-                    <Link
-                      href={link.href}
-                      className={`nav-link ${isActive && "active"}`}
-                    >
-                      {link.text}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-            {!user && (
+          {user && (
+            <>
               <button
-                className="btn btn-outline-primary"
-                onClick={() => router.push("/login")}
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
               >
-                Login
+                <span className="navbar-toggler-icon"></span>
               </button>
-            )}
-            {user && (
-              <button
-                className="btn btn-outline-primary"
-                onClick={() => router.push('/logout')}
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
               >
-                Logout
-              </button>
-            )}
-          </div>
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  {navLinks.map((link) => {
+                    const isActive = (pathName == link.href);
+                    return (
+                      <li className="nav-item" key={link.href}>
+                        <Link
+                          href={link.href}
+                          className={`nav-link ${isActive && "active"}`}
+                        >
+                          {link.text}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+                <button
+                  className="btn btn-outline-primary"
+                  onClick={() => router.push("/logout")}
+                >
+                  Logout
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </nav>
     </>
